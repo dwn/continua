a5007e8ea7576fbb9a2d848c826dead09f385090
dwn <danwnielsen@gmail.com>
2021-01-17 14:01:48 +0300

    update

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,18 @@
 $(function () {
+  function getParameterByName(name, url) {
+      if (!url) url = window.location.href;
+      name = name.replace(/[\[\]]/g, '\\$&');
+      var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+          results = regex.exec(url);
+      if (!results) return null;
+      if (!results[2]) return '';
+      return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  }
   var socket = io();
 ////////////////////////////////////////////
   $('form').submit(function(){
     if (!$('#m').val()) return false;
-    const urlParams = new URLSearchParams(window.location.search);
-    const username = urlParams.get('username');
-    const  = getParameterByName();
+    const username = getParameterByName('username');
     socket.emit('chat message', username + ':' + $('#m').val());
     $('#m').val('');
     return false;
