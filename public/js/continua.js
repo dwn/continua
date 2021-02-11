c7af9c3eda1fb9acb26e7385f37d0c6530813d1e
dwn <danwnielsen@gmail.com>
2021-02-11 13:18:30 -0600

    update

--- public/js/continua.js
+++ public/js/continua.js
@@ -1494,7 +1494,7 @@ function loadConscriptFont(family, addr) {
   loadTryForever(newFont).then(function(loadedFace) {
     setTimeout(function() { //Occasionally even after the font was successfully loaded, it needs a brief moment before adding
       document.fonts.add(loadedFace);
-    }, 200);
+    }, 500);
     document.getElementById('conscript-text').style.fontFamily = family;
     document.getElementById('conscript-text').innerText = tmp;
     conscriptTextReady = true;
