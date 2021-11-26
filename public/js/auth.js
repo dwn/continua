d12a416b675001c6fa1353637440cf7024519b4b
dwn <danwnielsen@gmail.com>
2021-11-26 01:40:13 -0600

    update

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,17 +1,19 @@
-  function onSuccess(googleUser) {
-    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-  }
-  function onFailure(error) {
-    console.log(error);
-  }
-  function renderButton() {
-    gapi.signin2.render('my-signin2', {
-      'scope': 'profile email',
-      'width': 240,
-      'height': 50,
-      'longtitle': true,
-      'theme': 'dark',
-      'onsuccess': onSuccess,
-      'onfailure': onFailure
-    });
-  }
+function onSuccess(googleUser) {
+  console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+  document.getElementById('signin-container').style.display = 'none';
+  document.getElementById('custom-select-element').style.display = 'inline-block';
+}
+function onFailure(error) {
+  console.log(error);
+}
+function renderButton() {
+  gapi.signin2.render('signin', {
+    'scope': 'profile email',
+    'width': 150,
+    'height': 75,
+    'longtitle': false,
+    'theme': 'dark',
+    'onsuccess': onSuccess,
+    'onfailure': onFailure
+  });
+}
--- public/js/continua.js
+++ public/js/continua.js
@@ -1389,8 +1389,8 @@ function loadConscriptFont(family, addr) {
 // CUSTOM SELECT DROPDOWN
 ////////////////////////////////////////////
 var x, i, j, selElmnt, a, b, c;
-//Look for any elements with the class 'custom-select'
-x = document.getElementsByClassName('custom-select');
+//Look for any elements with the class 'custom-select-element'
+x = document.getElementsByClassName('custom-select-element');
 for (i = 0; i < x.length; i++) {
   selElmnt = x[i].getElementsByTagName('select')[0];
   //For each element, create a new DIV that will act as the selected item
