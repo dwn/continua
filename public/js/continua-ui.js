7c9294f70f9a466a3dff1a3bf82ec3fccad4577d
dwn <danwnielsen@gmail.com>
2021-12-06 15:57:59 -0600

    update

--- public/js/continua-ui.js
+++ public/js/continua-ui.js
@@ -50,7 +50,7 @@ function expandTopRow() {
 }
 function invalidateMessagesWithOldFont(longId) {
   debug('invalidating old messages for '+longId)
-  let arrEl = $(`[style*="${longId}"]`).html('ᵖʳᶦᵒʳ ᶠᵒⁿᵗ').parent().css('color','rgba(0,0,0,.35)');
+  let arrEl = $(`[style*="${longId}"]`).css('font-family','Arial').html('prior font').parent().css('color','rgba(0,0,0,.35)');
 }
 ////////////////////////////////////////////
   function closeAllSelect(el, skipConfirm = false) {
